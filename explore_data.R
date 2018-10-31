b2f1c0a462d916ca9d78f460903fc21d52804065
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2018-10-31 18:32:12 -0400

    Examine proportion of days with missing data for each dyad..

--- explore_data.R
+++ explore_data.R
@@ -24,10 +24,54 @@ create_dyad_id <- function(data){
 dyad_steps_diffs <- lapply(dyad_steps_diffs, create_dyad_id)
 dyad_sleep_diffs <- lapply(dyad_sleep_diffs, create_dyad_id)
 
+# save these 
+saveRDS(dyad_steps_diffs, "dyad_steps_diffs.rds")
+saveRDS(dyad_sleep_diffs, "dyad_sleep_diffs.rds")
+
+
 # count how many were in study at same time
-same_time <- sum(sapply(dyad_steps_diffs, function(x) nrow(x) > 0))
+same_time_ <- sum(sapply(dyad_steps_diffs, function(x) nrow(x) > 0))
   # 354116
 
 # how many dyads have missing data, meaning both in study at same time
 # but one person (at least) has NA for day on given measure
-missing_data_days <- sapply(all_dyad_diffs_mc, function(x) sum(is.na(x['diff'])))
+missing_steps_days <- sapply(dyad_steps_diffs, function(x) sum(is.na(x['diff'])))
+missing_sleep_days <- sapply(dyad_sleep_diffs, function(x) sum(is.na(x['diff'])))
+
+# get proportion of dyad's common dates with missing data
+missing_steps_prop <- sapply(dyad_steps_diffs, function(x) sum(is.na(x['diff'])) / nrow(x))
+missing_sleep_prop <- sapply(dyad_sleep_diffs, function(x) sum(is.na(x['diff'])) / nrow(x))
+
+# visualizing missing proportions
+hist(missing_steps_prop)
+hist(missing_sleep_prop)
+
+# make tibble to be able to facet histogram
+steps_miss_prop_t <- as_tibble(missing_steps_prop) %>% 
+  rename(proportion = value) %>% 
+  mutate(activity = "steps")
+sleep_miss_prop_t <- as_tibble(missing_steps_prop) %>% 
+  rename(proportion = value) %>% 
+  mutate(activity = "sleep")
+missing_prop_combined <- bind_rows(steps_miss_prop_t,
+                                   sleep_miss_prop_t)
+
+# get mean for missing proportion types for histograms below
+mean_steps <- mean(steps_miss_prop_t$proportion, na.rm = TRUE)
+mean_sleep <- mean(steps_miss_prop_t$proportion, na.rm = TRUE)
+
+# plot histogram faceted
+missing_prop_combined %>% 
+  ggplot(aes(x = proportion, fill = as.factor(activity))) +
+  geom_histogram(alpha = 0.5) +
+  facet_wrap(~as.factor(activity)) +
+  labs(title = "Proportion of day data missing for each dyad",
+       x = "Proportion",
+       y = "Count") +
+  scale_fill_discrete(name = "Type of activity") +
+  geom_vline(data=filter(missing_prop_combined, as.factor(activity)=="steps"),
+             aes(xintercept=mean_steps), linetype = "dashed", colour="black") +
+  geom_vline(data=filter(missing_prop_combined, as.factor(activity)=="sleep"),
+             aes(xintercept=mean_sleep), linetype = "dashed", colour="black")
+  
+  
