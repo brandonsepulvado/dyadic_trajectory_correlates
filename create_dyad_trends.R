3681710ea4947fdb3016c8a6b790614985faee1e
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2018-10-31 12:47:27 -0400

    Create list with dyadic differences in steps.

--- create_dyad_trends.R
+++ create_dyad_trends.R
@@ -5,25 +5,85 @@
 # data comes from "data_import_clean.R"
 
 # get all combinations
-get_unique_dyads <- function(user_id_col, col_for_diffs){
+get_unique_dyads <- function(user_id_col){
   # get unique user ids in two columns
-  unique_ids_1 <- unqiue(user_id_col)
+  unique_ids_1 <- unique(user_id_col)
   unique_ids_2 <- unique(user_id_col)
   
   # get all possible combinations
-  user_combinations <- expand.grid(unique_ids_1, unique_ids_2) %>% 
+  dyad_list <- expand.grid(unique_ids_1, unique_ids_2) 
     # more informative names
-    rename(id1 = Var1,
-           id2 = Var2) %>% 
+    
+  dyad_list <- dyad_list %>% 
+    dplyr::rename(id1 = Var1,
+                 id2 = Var2) %>% 
     # remove cases where id1 and id2 are same person
-    filter(id1 != id2)
+    dplyr::filter(id1 != id2)
   
-  # 
+  dyad_list$id1 <- as.character(dyad_list$id1)
+  dyad_list$id2 <- as.character(dyad_list$id2)
   
+  # return dyad list
+  return(dyad_list)
 }
 
+
+
+
 # create difference vectors for all combinations
-get_trend_diffs <- function(data, ids, focal_var, date_var) {
+diff_trends <- function(dyad_list){
+  filtered <- fitbit_data %>% 
+    filter(participid ==  dyad_list[,1] | participid == dyad_list[,2]) %>% 
+    group_by(datadate) %>% 
+    filter(n_distinct(participid) == 2) %>% 
+    summarise(diff = max(steps) - min(steps)) %>% 
+    mutate(node1 =  dyad_list[,1],
+           node2 = dyad_list[,2]) %>% 
+    ungroup()
+  
+  #filtered <- data %>% 
+    #filter(data_id == single_dyad_df[1,1] | data_id == single_dyad_df[1,2]) 
+  
+  #filtered <- filtered %>% 
+    #group_by_(date) %>% 
+    #filter(n_distinct(data_id) == 2)
+  #%>% 
+    #group_by(date) %>% 
+    #filter(n_distinct(data_id) == 2) %>% 
+    #summarise(diff = max(focal_var) - min(focal_var)) %>% 
+    #ungroup()
+  return(filtered)
+}
+
+# make list
+dyad_as_list <- split(dyad_list, seq(nrow(dyad_list)))
+
+
+# apply to all dyads
+#all_dyad_diffs <- lapply(dyad_as_list[1:10], diff_trends)
+
+# apply using parallelization 
+all_dyad_diffs_mc <- parallel::mclapply(dyad_as_list, diff_trends, 
+                                        mc.cores = getOption("mc.cores", 10L))
+  # empty if dates don't overlap; NA if one had no values for day
+
+
+# save object
+saveRDS(all_dyad_diffs_mc, "all_dyad_diffs.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+get_trend_diffs <- function(ids, focal_var, date_var) {
   if(length(ids) < 2){
     stop("You have entered fewer than two user ids!")
   } else if(length(ids) > 2) {
@@ -33,10 +93,10 @@ get_trend_diffs <- function(data, ids, focal_var, date_var) {
   } else {
    
     # get date vectors for each user id
-    node1 <- dplyr::filter(data, participid == ids[1]) #%>% 
-      #dplyr::select(participid, datadate)
-    node2 <- dplyr::filter(data, participid == ids[2]) #%>% 
-      #dplyr::select(participid, datadate)
+    node1 <- dplyr::filter(data, participid == ids[1]) %>% 
+      filter(!is.na(focal_var))
+    node2 <- dplyr::filter(data, participid == ids[2])  %>% 
+      filter(!is.na(focal_var))
     
     # get intersection of dates
     common_dates <- intersect(node1$ddate_var, node2$date_var)
@@ -55,8 +115,14 @@ get_trend_diffs <- function(data, ids, focal_var, date_var) {
                     for one or more dates for either or both
                     individuals. Using unique() to remove duplicates.",
                     sep = " "))
-      node1 <- unique(node1)
-      node2 <- unique(node2)
+      node1 <- unique(node1$date_var, node1$focal_var)
+      node2 <- unique(node2$date_var, node2$focal_var)
+      
+      # check if still unequal 
+      if(nrow(node1) == nrow(node2)){
+        warning(paste(ids[1], "and", ids[2], "still have an unequal number of date
+                      observations!"))
+      }
     }
     
     # arrange in ascending dates
@@ -68,8 +134,71 @@ get_trend_diffs <- function(data, ids, focal_var, date_var) {
     # get difference vector
     diff_vector <- node1$focal_var - node2$focal_var
     
+   # add dates
+    diff_dates_vector <- bind_cols(node1$datadate, diff_vector)
+    
   }
 } 
+
+
+
+
+
+
+# attempt 2
+get_trend_diffs2 <- function(data, dyad, id_var, focal_var, date_var){
+  # dyad must be a tibble/df
+  
+  # get date vectors for each user id
+  node1 <- data %>% 
+    dplyr::filter(id_var == sa.character(dyad[1,1]) %>% 
+    dplyr::filter(!is.na(focal_var))
+  node2 <- data %>% 
+    dplyr::filter(id_var == as.character(dyad[1,2]) %>% 
+    dplyr::filter(!is.na(focal_var))
+  
+}
+    
+  # get intersection of dates
+  common_dates <- intersect(node1$date_var, node2$date_var)
+  
+  # keep only those dates in node1 and node2 that appear in both
+  node1 <- node1 %>% 
+    filter(date_var %in% common_dates)
+  node2 <- node2 %>% 
+    filter(date_var %in% common_dates)
+  
+  # arrange in ascending dates
+  node1 <- node1 %>% 
+    arrange(date_var)
+  node2 <- node2 %>% 
+    arrange(date_var)
+  
+  # get difference vector
+  diff_vector <- node1$focal_var - node2$focal_var
+  
+  # return differences
+  return(diff_vector)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # problems in getting daily pairwise differences
@@ -92,24 +221,29 @@ test_fitbit_data1 <- test_fitbit_data %>%
   
 ##### check for examples of duplicates
 # get date vectors for each user id
-node1 <- dplyr::filter(fitbit_data, participid == "3LZJFK") #%>% 
-#dplyr::select(participid, datadate)
-node2 <- dplyr::filter(fitbit_data, participid == "3M5ZZL") #%>% 
-#dplyr::select(participid, datadate)
-
+node1 <- dplyr::filter(fitbit_data, participid == "3LZJFK") %>% 
+  filter(!is.na(steps))
+node2 <- dplyr::filter(fitbit_data, participid == "3M5ZZL")  %>% 
+  filter(!is.na(steps))
 # get intersection of dates
 common_dates <- intersect(node1$datadate, node2$datadate)
 
 # keep only those dates in node1 and node2 that appear in both
 node1 <- node1 %>% 
-  filter(datadate %in% common_dates)
+  filter(datadate %in% common_dates) 
 node2 <- node2 %>% 
-  filter(datadate %in% common_dates)
+  filter(datadate %in% common_dates) 
 
 # node2 %>% arrange(datadate) %>% select(steps, datadate) %>% View()
 
 # this isgnores other DIFFERENT cell contents!!!
-node1_unique <- unique(node1$participid, node1$steps) 
-node2_unique <- unique(node2$participid, node2$steps)
+node1_unique <- unique(node1$participid, node1$steps) %>% 
+  arrange(datadate)
+node2_unique <- unique(node2$participid, node2$steps) %>% 
+  arrange(datadate)
 
 setequal(nrow(node1_unique), nrow(node2_unique))
+
+test_vector <- node1_unique
+
+
