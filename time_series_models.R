a3d70cf6e94bb5f25e993b8e6f86cf33021d9146
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2018-11-22 21:59:06 -0600

    Create file for time series models code.

---
+++ time_series_models.R
@@ -0,0 +1,8 @@
+#####
+##### run time series models
+#####
+
+###
+# load data if needed (uncomment)
+#yad_sleep_diffs <- readRDS("dyad_sleep_diffs.rds")
+#dyad_steps_diffs <- readRDS("dyad_steps_diffs.rds")
