5e99588447e0d0bfa17eb2aac2b3ec8ad8502f23
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-01-25 16:36:54 -0500

    Evaluate performance across k values (for diffs, check underscore versus hyphen)

---
+++ code/k2_25.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -M bsepulva@nd.edu
+#$ -m abe
+#$ -pe smp 15
+#$ -q long
+
+module load gcc
+module load R/3.6.0
+setenv R_LIBS /scratch365/dheryadi/R-3.6
+R CMD BATCH k2_25.R  k2_25.out
