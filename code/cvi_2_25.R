649b2b634628d5edcbe340ef48c92e110c1f4eb3
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-02-09 14:57:29 -0500

    changing number of cores so ND CRC does not kill job.

--- code/cvi_2_25.R
+++ code/cvi_2_25.R
@@ -8,7 +8,7 @@ library(here)
 library(purrr)
 
 # create multi-process workers
-workers <- makeCluster(12)
+workers <- makeCluster(8)
 
 # load dtwcluster in each one, and make them use 1 thread per worker
 invisible(clusterEvalQ(workers, {
@@ -21,7 +21,10 @@ require(doParallel)
 registerDoParallel(workers)
 
 # load data (output from k2_25)
-test_k <- readRDS(file = here('Private', 'nethealth', 'k_2-25.rds'))
+test_k <- readRDS(file = here('Private', 'nethealth', 'k_2-25_20200203.rds'))
+
+# get class
+class(test_k)
 
 # get tibble of cvi values for different k
 eval_k <- sapply(test_k, cvi, type = 'internal') 
@@ -39,4 +42,4 @@ eval_k <- eval_k %>%
   gather(key = 'number_k', value = 'value', -c(index, goal))
 
 # save results
-saveRDS(eval_k, file = here::here('Private', 'nethealth', 'cvi_2_25.rds'))
+saveRDS(eval_k, file = here::here('Private', 'nethealth', 'cvi_2_25_20200203.rds'))
