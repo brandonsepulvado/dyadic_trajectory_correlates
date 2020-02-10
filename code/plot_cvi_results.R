e25b1ed5a523fac048fe2319a2a17f364fffca86
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-02-09 19:34:42 -0500

    Including code to interface directly with the previous file.

--- code/plot_cvi_results.R
+++ code/plot_cvi_results.R
@@ -1,4 +1,6 @@
-# goal : visualize the results of the cluster validity indices
+# ==============================================================================
+# visualize the results of the cluster validity indices
+# ==============================================================================
 
 # run after cvi_2_25.R
 # note that it stopped running after sapply()
@@ -8,6 +10,19 @@ eval_k <- readRDS(here::here('output', 'cvi_2_25_20200203.rds'))
 
 # get average across five runs
 eval_k_mean5 <- (eval_k[,1:24] + eval_k[,25:48] + eval_k[,49:72] + eval_k[,73:96] + eval_k[,97:120])/5
+
+# make into tibble with min/max variable
+eval_k_mean5 <- eval_k_mean5 %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column(var = 'index') %>%
+  # have minize or maximize variable
+  mutate(goal = case_when(index %in% c('COP', 'DB', "DBstar") ~ 'minimize',
+                          TRUE ~ 'maximize'))
+
+# gather for plotting
+eval_k_mean5 <- eval_k_mean5 %>%
+  gather(key = 'number_k', value = 'value', -c(index, goal))
+
 
 # plot results
 eval_k_mean5 %>%
